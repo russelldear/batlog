764ea83604dcfdb21921526fd0db60bb0d88d871
Russell Dear <russell.dear@gmail.com>
2019-07-20 12:37:48 +1200

    cron to metric logger

--- battest.sh
+++ battest.sh
@@ -1,2 +1,13 @@
-date >> ~/batlog.dat
-/usr/sbin/ioreg -l | egrep "CycleCount|Capacity" >> ~/batlog.dat
+dateNow=$(date '+%F %T')
+
+current=$(/usr/sbin/ioreg -l | egrep "CurrentCapacity")
+current=${current#*=}
+
+curl -d "{ \"metrics\": [{\"name\":\"MacbookProCurrentCapacity\", \"value\":\"$current\", \"timestamp\":\"$dateNow\"}] }" -H "Content-Type: application/json" -X POST https://***.execute-api.us-east-1.amazonaws.com/v1/metrics
+
+maxCap=$(/usr/sbin/ioreg -l | egrep "MaxCapacity")
+maxCap=${maxCap#*=}
+
+curl -d "{ \"metrics\": [{\"name\":\"MacbookProMaxCapacity\", \"value\":\"$maxCap\", \"timestamp\":\"$dateNow\"}] }" -H "Content-Type: application/json" -X POST https://***.execute-api.us-east-1.amazonaws.com/v1/metrics
+
+
